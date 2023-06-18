af10c6e44e485088715f3d602d390953322bf544
Van-tesh <121645908+Van-tesh@users.noreply.github.com>
2023-06-18 21:07:29 +0300

    C program using puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * code to output "programming is like ...
  */
 
-int main (void)
+int main(void)
 {
-puts ("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
