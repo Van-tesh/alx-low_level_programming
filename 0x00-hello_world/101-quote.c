cb55580219e4881dab5708eff9b9fe94e1556206
Van-tesh <121645908+Van-tesh@users.noreply.github.com>
2023-06-18 21:59:08 +0300

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - entry point
- * Return: 1
+ * Return: 1 (Success)
  */
 int main(void)
 {
-fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
