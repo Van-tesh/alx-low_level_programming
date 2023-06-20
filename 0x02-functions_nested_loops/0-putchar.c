b808cf5012416f2058e85196db2dc339c7ea6943
Vantesh <121645908+Vantesh@users.noreply.github.com>
2023-06-20 21:40:47 +0300

    fix to pass checker

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <main.h>
 /**
  * main - print _putchar
  * Return: 0 if successful
  */
 int main (void)
 {
-write("_putchar");
+write("_putchar\n");
 return (0);
 
 
